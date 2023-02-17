6804dfbfb810e7626b65d434e23baed7a0e637e4
Adhicitta Masran <adhicittamasran@gmail.com>
2023-02-17 23:26:47 +0800

    set singular table naming

--- src/config/database/mysql.go
+++ src/config/database/mysql.go
@@ -8,13 +8,18 @@ import (
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
 )
 
 func InitMySQL() *gorm.DB {
 	config := env.GetSQLEnv()
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
 		config.User, config.Password, config.Host, config.Port, config.DBName)
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true,
+		},
+	})
 	if err != nil {
 		log.Fatal("error connecting to the database: ", err)
 	}
