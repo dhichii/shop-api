79b4def81ca8447a1768f9bf52a39c4669d5f392
Adhicitta Masran <adhicittamasran@gmail.com>
2023-02-17 18:38:17 +0800

    add response template

---
+++ src/helper/response.go
@@ -0,0 +1,47 @@
+package helper
+
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type (
+	Response struct {
+		Status  bool        `json:"status"`
+		Message string      `json:"message"`
+		Errors  []string    `json:"errors"`
+		Data    interface{} `json:"data"`
+	}
+
+	ResponseParam struct {
+		Ctx      fiber.Ctx
+		HttpCode int
+		Method   string
+		Errors   []string
+		Data     interface{}
+	}
+)
+
+func SuccessResponse(param ResponseParam) error {
+	message := fmt.Sprintf("Success to %s Data", param.Method)
+	return param.Ctx.Status(param.HttpCode).
+		JSON(
+			Response{
+				Status:  true,
+				Message: message,
+				Data:    param.Data,
+			},
+		)
+}
+
+func FailedResponse(param ResponseParam) error {
+	message := fmt.Sprintf("Failed to %s Data", param.Method)
+	return param.Ctx.Status(param.HttpCode).
+		JSON(
+			Response{
+				Message: message,
+				Errors:  param.Errors,
+			},
+		)
+}
